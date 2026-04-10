57ca6f2b7f68a497e70bd5d4736e8ab65a5254f5
Егор <lxstlaudexc@gmail.com>
2026-04-10 17:58:41 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,18 @@
+﻿package main
+
+import (
+	"log"
+
+	apihttp "restAPI/http"
+	"restAPI/todo"
+)
+
+func main() {
+	todoList := todo.NewList()
+	handlers := apihttp.NewHTTPHandlers(todoList)
+	server := apihttp.NewHTTPServer(handlers)
+
+	if err := server.StartServer(); err != nil {
+		log.Fatalln(err)
+	}
+}
